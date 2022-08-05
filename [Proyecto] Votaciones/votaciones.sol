b50928500fd96460b5050aef257ab9d4d32a3a75
pugafran <franciscplgi36@gmail.com>
2022-08-05 17:29:59 +0200

    ERC20

--- [Proyecto] Votaciones/votaciones.sol
+++ [Proyecto] Votaciones/votaciones.sol
@@ -15,7 +15,35 @@ contract votacion{
     mapping (string => uint) votos_candidato;
 
     string [] candidatos;
+    byte32 [] votantes;
 
+    function setCandidato(string memory _nombre, uint _edad, string memory _id) public{
+        
+        id_candidato[_nombre] = keccak256(abi.encodePacked(_nombre, _edad, _id));
+        candidatos.push(_nombre);
+        
     
+    }
+
+    function getCandidatos() public view returns(string [] memory) {
+        return candidatos;
+    }
+
+    function setVotos(string memory _nombre) public{
+        
+        for(uint i = 0; i < votantes.length; i++){
+            require(votantes[i] != keccak256(abi.encodePacked(msg.sender), "Ya has votado previamente"));
+        }
+
+        votantes.push(keccak256(abi.encodePacked(msg.sender)));
+
+        votos_candidato[_nombre]++;
+        
+    
+    }
+
+    function getVotos(string memory _nombre) public view returns (uint){
+        return votos_candidato[_nombre];
+    }
 
 }
